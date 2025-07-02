f54860ffe31d9f281fe41fd598fcf311fd3dde50
Kamlesh Nagar <49498315+kamleshnagar@users.noreply.github.com>
2025-07-02 12:45:40 +0530

    Update idiscuss .sql

--- mysql/idiscuss .sql
+++ mysql/idiscuss .sql
@@ -18,7 +18,9 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `idiscuss`
+CREATE DATABASE IF NOT EXISTS `idiscuss`;
+-- Database: `idiscuss`;
+
 --
 
 -- --------------------------------------------------------
